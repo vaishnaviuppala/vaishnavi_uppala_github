ec760eaab0804a0b233d94212af6ed3a2b3e20d8
Vaishnavi Uppala <143928981+vaishnaviuppala@users.noreply.github.com>
2023-09-04 14:17:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from "./Components/Nav";
+
+const App = () => {
+    return(
+        <div>
+            <Nav />
+        </div>
+    )
+}
+
+export default App
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,211 @@
+import React from "react";
+import Naav from './Naav.css';
+import image from "./main bg.png";
+import img1 from "./t1.png";
+import img2 from "./t3.png";
+import img3 from "./glob.png";
+import img4 from "./ctr1.png";
+import img5 from "./i22.png";
+import img6 from "./top11.png";
+import img7 from "./top12.png";
+import img8 from "./coll.png";
+import img9 from "./80.png";
+import img10 from "./80bilo.png";
+import img11 from "./invest.png";
+import img12 from "./security.png";
+import img13 from "./3thok.png";
+import img14 from "./56mil.png";
+import img15 from "./srror.png";
+import img16 from "./secr.png";
+import img17 from "./depen.png";
+import img18 from "./globee.png";
+
+
+
+var Nav = () => {
+    return(
+        <div>
+                
+
+            <img src={image} alt="" id="bg1" />
+            
+            <a href="https://github.com/features"><p id="v1">Product ›</p></a>
+            <a href="https://support.github.com/?tags=dotcom-footer"><p id="v2">Solution ›</p></a>
+            <a href="https://resources.github.com/"><p id="v3">Open Source ›</p></a>
+            <a href="https://docs.github.com/en/site-policy/github-terms/github-terms-of-service"><p id="v4">Pricing ›</p></a>
+            <div id="v5"><input type="search" name="" id="v51" placeholder="Search or Jump to..."/></div>
+            <div id="v6"><a href="https://github.com/login"><p id="v61">Sign in</p></a></div>
+            <div id="v7"><a href="https://github.com/signup?ref_cta=Sign+up&ref_loc=header+logged+out&ref_page=%2F&source=header-home"><p id="v71">Sign Up</p></a></div>
+
+
+
+            <a href="https://www.bing.com/ck/a?!&&p=b3d2fd48c9731a7dJmltdHM9MTY5MzY5OTIwMCZpZ3VpZD0zZjEzYWMzMi00ODRiLTZiMTAtMTQyMi1iZTlhNGM0YjZkNGUmaW5zaWQ9NTIxNw&ptn=3&hsh=3&fclid=3f13ac32-484b-6b10-1422-be9a4c4b6d4e&psq=github&u=a1aHR0cHM6Ly9naXRodWIuY29tLw&ntb=1"><img src="https://github.githubassets.com/images/modules/site/icons/footer/github-mark.svg" alt="" id="icon" /></a>
+            <img src="https://github.githubassets.com/images/modules/site/home-campaign/hero-drone.webp" alt="" id="drone" />
+            <img src="https://github.githubassets.com/images/modules/site/home-campaign/lines-hero.svg" alt="" id="thrd1" />
+
+            <a href="https://githubuniverse.com/?utm_source=github&utm_medium=banner&utm_campaign=bannerlo">
+            <div id="srch1">
+            <img src="https://github.githubassets.com/images/modules/site/universe/eyebrow-23@2x.png?width=44&format=webpll" alt="" id="ic2"/>
+            <p id="h11">GetHub Universe:Drive in to AI,Security,DevEx</p>
+            <p id="h22">Get your Tickets now to join us on Nov.8-9</p>
+            <p id="arro">-</p>
+            </div></a>
+
+            <h1 id="hl">Let's build from here</h1>
+            <h2 id="hl2">The AI-powered developer platform to build, scale, and deliver <br></br>
+             secure software.</h2>
+             <input type="text" name="" id="mail" placeholder="Mail Adderss" />
+             <a href="https://github.com/signup?user_email=&source=form-home-signup"><button id="b1">Sign up for GitHub</button></a>
+             <p id="spc"> | </p>
+
+             <div id="starta"><a href="https://github.com/organizations/enterprise_plan?ref_cta=Start+a+free+enterprise+trial&ref_loc=Home+campaign+hero&ref_page=%2F"><p id="startas">Start a free enterprise trial  -----</p></a></div>
+
+            <p id="trusted">Trusted by the world’s leading organizations ↘️</p>
+            <img src="https://github.githubassets.com/images/modules/site/home-campaign/logos/stripe.svg" alt="" id="simg1" />
+            <img src="https://github.githubassets.com/images/modules/site/home-campaign/logos/pinterest.svg" alt="" id="simg2" />
+            <img src="https://github.githubassets.com/images/modules/site/home-campaign/logos/kpmg.svg" alt="" id="simg3" />
+            <img src="https://github.githubassets.com/images/modules/site/home-campaign/logos/mercedes.svg" alt="" id="simg4" />
+            <img src="https://github.githubassets.com/images/modules/site/home-campaign/logos/pg.svg" alt="" id="simg5" />
+            <img src="https://github.githubassets.com/images/modules/site/home-campaign/logos/telus.svg" alt="" id="simg6" />
+
+            <img src={img1} alt="" id="t1" />
+
+            <div id="d2">
+                <img src={img2} alt="" id="t3" />
+                <p id="prohead">Productivity</p>
+                <p id="acc">Accelerate high-quality software </p>
+                <p id="deve">development.</p>
+                <p id="our"> Our AI-powered platform <b></b></p> 
+                <p id="dri">drives innovation with tools that boost <b></b> developer velocity.</p>
+
+                <img src={img3} alt="" id="glob" />
+                <img src={img4} alt="" id="img4" />
+                <img src="https://github.githubassets.com/images/modules/site/home-campaign/footer-galaxy.jpg" alt="" id="starbg" />
+                <img src="https://github.githubassets.com/images/modules/site/codespaces/illo-ports.png" alt="" id="web3000" />
+                <img src="https://github.githubassets.com/images/modules/site/codespaces/illo-context-menu.png" alt="" id="web300" />
+                <img src="https://github.githubassets.com/images/modules/site/codespaces/illo-cursor.png" alt="" id="webmouse" />
+                <nav id="subnav">GitHub Codespaces </nav>
+                <nav id="subnav0">offers a complete dev </nav> 
+                <nav id="subnav1"> environment in seconds, so you can code,</nav>
+                <nav id="subnav2"> build, test, and open pull requests from any</nav>
+                <nav id="subnav3">repo anywhere.</nav>
+                <a href="https://github.com/features/codespaces"><nav id="subnav4">Check out GitHub Codespaces --</nav></a>
+                <img src={img5} alt="" id="img5" />
+                <img src={img6} alt="" id="img6" />
+                <img src={img7} alt="" id="img7" />
+                <img src={img8} alt="" id="img8" />
+
+                <img src="https://github.githubassets.com/images/modules/site/issues/illo/issues-plan.png?width=1500&format=webpll" alt="" id="img9" />
+                <img src={img9} alt="" id="img10" />
+                <img src="https://github.githubassets.com/images/modules/site/home-campaign/footer-galaxy.jpg" alt=""  id="img12" />
+                <img src="https://github.githubassets.com/images/modules/site/home-campaign/illu-projects.png" alt="" id="img11" />
+                <img src={img10} alt="" id="img13" />
+                <img src={img11} alt="" id="img14" />
+                <img src={img12} alt="" id="img15" />
+                <img src={img13} alt="" id="img16" />
+                <img src={img14} alt="" id="img17" />
+
+                <a href="https://github.com/features/copilot"><div id="acc4"></div></a>
+                <a href="https://github.com/features/actions"><div id="acc5"></div></a>
+                <a href="https://resources.github.com/security/sast/"><div id="acc6"></div></a>
+
+                <a href="https://github.com/features/issues"><div id="acc7"></div></a>
+
+
+                <a href="https://github.com/features/security/code"><div id="acc1"></div></a>
+                <a href="https://github.com/features/security/software-supply-chain"><div id="acc2"></div></a>
+                <a href="https://resources.github.com/security/sast/"><div id="acc3"></div></a>
+
+                <a href="https://github.com/features/security"><div id="a1_56"></div></a>
+                <img src="https://github.githubassets.com/images/modules/site/home-campaign/footer-galaxy.jpg" alt="" id="img18"/>
+                <img src="https://github.githubassets.com/images/modules/site/home-campaign/footer-galaxy.jpg" alt="" id="img19"/>
+                <img src="https://github.githubassets.com/images/modules/site/home-campaign/footer-galaxy.jpg" alt="" id="img20"/>
+                <img src={img15} alt="" id="img21"/>
+                <img src={img16} alt="" id="img22" />
+                <a href="https://github.com/features/security/code"><div id="a2"></div></a>
+                <img src={img17} alt="" id="img23" />
+                <div id="a3"></div>
+                <div id="a4"></div>
+                <img src="https://github.githubassets.com/images/modules/site/home-campaign/footer-galaxy.jpg" alt="" id="lstbg03"/>
+
+                <h2 id="l2i">The place for anyone from anywhere</h2>
+                <h3 id="l3i">to build anything</h3>
+                <p id="lstp1">Whether you’re scaling your startup or just learning how to code, GitHub is</p>
+                <p id="lstp2">your home. Join the world’s largest developer platform to build the</p> 
+                <p id="lstp3">innovations that empower humanity. Let’s build from here.</p>
+
+                <a href="https://github.com/signup?ref_cta=Sign+up+for+GitHub&ref_loc=Home+campaign+footer&ref_page=%2F"><div id="sand"><p id="sanpd">SignUp for GitHub</p></div></a>
+                <a href="https://github.com/organizations/enterprise_plan?ref_cta=Start+a+free+enterprise+trial&ref_loc=Home+campaign+footer&ref_page=%2F"><div id="sane"><p id="sanpe">Start a free enterprise rtial</p></div></a>
+                <img src={img18} alt="" id="globie" />
+
+
+
+                <div id="bod">
+                    <p id="k1">1. The Total Economic Impact™ Of GitHub Enterprise Cloud and Advanced Security, a commissioned study conducted by Forrester Consulting, 2022. Results are for a composite organization based on interviewed customers.</p>
+                    <p id="k2">2. GitHub, Octoverse 2022 The state of open source software.</p>
+                    <a href="https://www.bing.com/ck/a?!&&p=ca180e60cea65902JmltdHM9MTY5MzY5OTIwMCZpZ3VpZD0zZjEzYWMzMi00ODRiLTZiMTAtMTQyMi1iZTlhNGM0YjZkNGUmaW5zaWQ9NTIxOA&ptn=3&hsh=3&fclid=3f13ac32-484b-6b10-1422-be9a4c4b6d4e&psq=github&u=a1aHR0cHM6Ly9naXRodWIuY29tLw&ntb=1"><h3 id="k3">GitHub</h3></a>
+                    <p id="k4">Subscribe to The GitHub Insider</p>
+                    <p id="k5">Discover tips, technical guides, and best practices in our monthly</p>
+                    <p id="k6">newsletter for developers.</p>
+                   <a href="https://resources.github.com/newsletter/"><div id="k7"><p id="k71">Subscribe</p></div></a>
+
+                   <p id="k8">Product</p>
+                   <a href="https://github.com/features"><p id="k9">Features</p></a>
+                   <a href="https://github.com/security"><p id="k10">Security</p></a>
+                   <a href="https://github.com/team"><p id="k11">Team</p></a>
+                   <a href="https://github.com/enterprise"><p id="k12">Enterprice</p></a>
+                   <a href="https://github.com/customer-stories?type=enterprise"><p id="k13">Customer Stories</p></a>
+                   <a href="https://github.com/readme"><p id="k14">The ReadMe Project</p></a>
+                   <a href="https://github.com/pricing"><p id="k15">Princing</p></a>
+                   <a href="https://resources.github.com/"><p id="k16">Resources</p></a>
+                   <a href="https://github.com/github/roadmap"><p id="k17">Roadmap</p></a>
+                   <a href="https://resources.github.com/devops/tools/compare/"><p id="k18">Compare GitHub</p></a>
+
+                   <p id="k19">Plateform</p>
+                   <a href="https://docs.github.com/en/get-started/exploring-integrations/about-building-integrations"><p id="k20">Developer API</p></a>
+                   <a href="https://partner.github.com/"><p id="k21">Partners</p></a>
+                   <a href="https://www.electronjs.org/"><p id="k22">Electron</p></a>
+                   <a href="https://desktop.github.com/"><p id="k23">GitHub Desktop</p></a>
+
+                   <p id="k24">Support</p>
+                   <a href="https://docs.github.com/en"><p id="k25">Does</p></a>
+                   <a href="https://github.com/orgs/community/discussions/"><p id="k26">Community Form</p></a>
+                   <a href="https://github.com/services/"><p id="k27">Professional Service</p></a>
+                   <a href="https://github.com/premium-support"><p id="k28">Premium Support</p></a>
+                   <a href="https://skills.github.com/"><p id="k29">Skills</p></a>
+                   <a href="https://www.githubstatus.com/"><p id="k30">Status</p></a>
+                   <a href="https://support.github.com/?tags=dotcom-footer"><p id="k31">Contact GitHub</p></a>
+
+                   <p id="k32">Company</p>
+                   <a href="https://github.com/about"><p id="k33">About</p></a>
+                   <a href="https://github.blog/"><p id="k34">Blog</p></a>
+                   <a href="https://github.com/about/careers"><p id="k35">Careers</p></a>
+                   <a href="https://github.com/about/press"><p id="k36">Press</p></a>
+                   <a href="https://github.com/about/diversity"><p id="k37">Inclusion</p></a>
+                   <a href="https://socialimpact.github.com/"><p id="k38">Social Impact</p></a>
+                   <a href="https://www.thegithubshop.com/"><p id="k39">Shop</p></a>
+
+                    <div id="bod2">
+                        <p id="k40">© 2023 GitHub, Inc.</p>
+                        <a href="https://docs.github.com/en/site-policy/github-terms/github-terms-of-service"><p id="k41">Terms</p></a>
+                        <a href="https://docs.github.com/en/site-policy/github-terms/github-terms-of-service"><p id="k42">Privacy</p></a>
+                        <a href="https://github.com/github/site-policy/pull/582"><p id="k43">(Updated 08/2022)</p></a>
+                        <a href="https://github.com/sitemap"><p id="k44">Sitemap</p></a>
+                        <a href="https://github.com/git-guides"><p id="k45">What is fit?</p></a>
+                        <a href="https://twitter.com/github"><img src="https://github.githubassets.com/images/modules/site/icons/footer/twitter.svg" alt="" id="k46" /></a>
+                        <a href="https://www.facebook.com/GitHub"><img src="https://github.githubassets.com/images/modules/site/icons/footer/facebook.svg" alt="" id="k47" /></a>
+                        <a href="https://www.linkedin.com/company/github"><img src="https://github.githubassets.com/images/modules/site/icons/footer/linkedin.svg" alt="" id="k48" /></a>
+                        <a href="https://www.youtube.com/github"><img src="https://github.githubassets.com/images/modules/site/icons/footer/youtube.svg" alt="" id="k49" /></a>
+                        <a href="https://www.twitch.tv/github"><img src="https://github.githubassets.com/images/modules/site/icons/footer/twitch.svg" alt="" id="k50" /></a>
+                        <a href="https://www.tiktok.com/notfound"><img src="https://github.githubassets.com/images/modules/site/icons/footer/tiktok.svg" alt="" id="k51" /></a>
+                        <a href="https://github.com/github"><img src=" https://github.githubassets.com/images/modules/site/icons/footer/github-mark.svg" alt="" id="k52" /></a>
+                        
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Nav;
